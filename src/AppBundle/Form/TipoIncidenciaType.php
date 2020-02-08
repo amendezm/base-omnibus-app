273e2fafa19a6cfbb47040bb87d4d166fee13a64
Alexis David <alexis.david.mendez@gmail.com>
2020-02-08 18:18:50 -0500

    Add description field to incidence types forms

--- src/AppBundle/Form/TipoIncidenciaType.php
+++ src/AppBundle/Form/TipoIncidenciaType.php
@@ -5,6 +5,8 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 
 class TipoIncidenciaType extends AbstractType
 {
@@ -16,9 +18,9 @@ class TipoIncidenciaType extends AbstractType
     {
         $builder
             ->add('tipoIncidencias')
-        ;
+            ->add('descripcion', TextareaType::class);
     }
-    
+
     /**
      * @param OptionsResolver $resolver
      */
