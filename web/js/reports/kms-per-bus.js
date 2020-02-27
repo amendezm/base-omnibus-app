f3f3202ca2ab0becf5834058e1241ddfa7104afb
Alexis David <alexis.david.mendez@gmail.com>
2020-02-26 21:03:02 -0500

    Allow filter bus by kilometers

--- web/js/reports/kms-per-bus.js
+++ web/js/reports/kms-per-bus.js
@@ -1,13 +1,49 @@
 window.onload = () => {
   const busNumberInput = document.getElementById("busNumberFilter");
+  const minKilometersFilter = document.getElementById("minKilometersFilter");
+  const maxKilometersFilter = document.getElementById("maxKilometersFilter");
   const busContainer = document.querySelector(".bus-container");
 
+  let busNumber = "";
+  let minKilometers = 0;
+  let maxKilometers = Number.MAX_VALUE;
+
   busNumberInput.onkeyup = event => {
+    handleChange(event.target.value);
+  };
+
+  minKilometersFilter.onchange = event => {
+    minKilometers = event.target.value || 0;
+    handleChange(busNumber);
+  };
+
+  minKilometersFilter.onkeyup = event => {
+    minKilometers = event.target.value || 0;
+    handleChange(busNumber);
+  };
+
+  maxKilometersFilter.onchange = event => {
+    maxKilometers = event.target.value || Number.MAX_VALUE;
+    handleChange(busNumber);
+  };
+
+  maxKilometersFilter.onkeyup = event => {
+    maxKilometers = event.target.value || Number.MAX_VALUE;
+    handleChange(busNumber);
+  };
+
+  const handleChange = numberFilterValue => {
+    busNumber = numberFilterValue;
     const size = busContainer.children.length;
     for (let i = 0; i < size; i++) {
       const node = busContainer.children.item(i);
-      console.info(node.getAttribute("id"));
-      if (!node.getAttribute("id").includes(event.target.value)) {
+      const busNumber = node.getAttribute("data-number");
+      const busKilometers = node.getAttribute("data-kilometers");
+      if (
+        !busNumber.includes(numberFilterValue) ||
+        Number.parseInt(busKilometers) < minKilometers ||
+        Number.parseInt(busKilometers) > maxKilometers
+      ) {
         node.classList.add("hide-element");
       } else {
         node.classList.remove("hide-element");
