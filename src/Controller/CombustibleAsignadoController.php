0f8b5c132d0065fe7d1440209c38f20cc9980024
Alexis David <alexis.david.mendez@gmail.com>
2020-05-31 18:10:19 -0400

    Configure routes, form, and views of the new entity

---
+++ src/Controller/CombustibleAsignadoController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\CombustibleAsignado;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CombustibleAsignadoController extends AbstractController
+{
+
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $asignaciones = $em->getRepository('App:CombustibleAsignado')->findAll();
+
+        return $this->render('combustible_asignado/index.html.twig', array(
+            'asignaciones' => $asignaciones,
+        ));
+    }
+
+    public function newAction(Request $request)
+    {
+        $combustibleAsignado = new CombustibleAsignado();
+        $form = $this->createForm('App\Form\CombustibleAsignadoType', $combustibleAsignado);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($combustibleAsignado);
+            $em->flush();
+
+            $this->addFlash('notice', 'Se ha insertado correctamente!');
+
+            return $this->redirectToRoute('combustible_asignado_index', array('id' => $combustibleAsignado->getId()));
+        } else if ($form->isSubmitted() && !$form->isValid()) {
+            $this->addFlash('error', 'Ha ocurrido algún error al insertar');
+        }
+
+        return $this->render('combustible_asignado/new.html.twig', array(
+            'combustibleAsignado' => $combustibleAsignado,
+            'form' => $form->createView(),
+        ));
+    }
+
+
+    public function editAction(Request $request, CombustibleAsignado $combustibleAsignado)
+    {
+        $editForm = $this->createForm('App\Form\CombustibleAsignadoType', $combustibleAsignado);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($combustibleAsignado);
+            $em->flush();
+
+            $this->addFlash('notice', 'Se ha editado correctamente!');
+
+            return $this->redirectToRoute('combustible_asignado_index', array('id' => $combustibleAsignado->getId()));
+        } else if ($editForm->isSubmitted() && !$editForm->isValid()) {
+            $this->addFlash('error', 'Ha ocurrido algún error al editar');
+        }
+
+        return $this->render('combustible_asignado/edit.html.twig', array(
+            'combustibleAsignado' => $combustibleAsignado,
+            'edit_form' => $editForm->createView(),
+        ));
+    }
+
+    public function deleteAction(Request $request, CombustibleAsignado $combustibleAsignado)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($combustibleAsignado);
+        $em->flush();
+
+        $this->addFlash('notice', 'Se ha borrado correctamente!');
+
+        return $this->redirectToRoute('combustible_asignado_index');
+    }
+}
---
+++ src/Form/CombustibleAsignadoType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+
+
+class CombustibleAsignadoType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('tarjetaCombustible', EntityType::class, array('class' => 'App\Entity\Tarjeta_combustible', 'multiple' => false, 'expanded' => false))
+            ->add('cantAsignada')
+            ->add('fecha', DateType::class, [
+                'widget' => 'single_text',
+                'html5' => false,
+                'attr' => ['data-language' => 'es', 'class' => 'datepicker-here', 'data-date-format' => 'yyyy-mm-dd', 'autocomplete' => 'off'],
+            ]);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\Entity\CombustibleAsignado',
+        ));
+    }
+}
